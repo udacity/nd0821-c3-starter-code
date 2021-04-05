98139b48a693578be57fc3f3a50d9d89181636e8
Justin Clifford Smith <32110303+JustCliffSmith@users.noreply.github.com>
2021-04-04 20:11:44 -0700

    Commit of top level starter files.

---
+++ starter/main.py
@@ -0,0 +1,1 @@
+# Put the code for your API here.
---
+++ starter/setup.py
@@ -0,0 +1,8 @@
+import setuptools
+
+setuptools.setup(
+    name="starter",
+    version="0.0.0",
+    description="Starter code.",
+    author="Student",
+)

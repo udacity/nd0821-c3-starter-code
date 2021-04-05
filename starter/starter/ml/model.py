1e5e6c23c6cc1a573c5288f59321710fbd37e47d
Justin Clifford Smith <32110303+JustCliffSmith@users.noreply.github.com>
2021-04-04 20:14:41 -0700

    Core module starter code.

---
+++ starter/starter/ml/model.py
@@ -0,0 +1,60 @@
+from sklearn.metrics import fbeta_score, precision_score, recall_score
+
+
+# Optional: implement hyperparameter tuning.
+def train_model(X_train, y_train):
+    """
+    Trains a machine learning model and returns it.
+
+    Inputs
+    ------
+    X_train : np.array
+        Training data.
+    y_train : np.array
+        Labels.
+    Returns
+    -------
+    model
+        Trained machine learning model.
+    """
+
+    pass
+
+
+def compute_model_metrics(y, preds):
+    """
+    Validates the trained machine learning model using precision, recall, and F1.
+
+    Inputs
+    ------
+    y : np.array
+        Known labels, binarized.
+    preds : np.array
+        Predicted labels, binarized.
+    Returns
+    -------
+    precision : float
+    recall : float
+    fbeta : float
+    """
+    fbeta = fbeta_score(y, preds, beta=1, zero_division=1)
+    precision = precision_score(y, preds, zero_division=1)
+    recall = recall_score(y, preds, zero_division=1)
+    return precision, recall, fbeta
+
+
+def inference(model, X):
+    """ Run model inferences and return the predictions.
+
+    Inputs
+    ------
+    model : ???
+        Trained machine learning model.
+    X : np.array
+        Data used for prediction.
+    Returns
+    -------
+    preds : np.array
+        Predictions from the model.
+    """
+    pass

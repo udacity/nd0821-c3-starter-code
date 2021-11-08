fdda1864a4102375e3a0147f4eff8d478b859ad5
Marcus Holmgren <marcus.holmgren1@gmail.com>
2021-11-08 20:36:44 +0100

    add body examples to API

--- starter/main.py
+++ starter/main.py
@@ -1,8 +1,8 @@
-# Put the code for your API here.
+"""FastAPI app for making prediction on census model"""
 import os
 import logging
 import pandas as pd
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 from pydantic import BaseModel, Field
 from joblib import load
 
@@ -12,20 +12,21 @@ from starter.starter.ml.model import inference
 logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
 logger = logging.getLogger()
 
-
 if "DYNO" in os.environ and os.path.isdir(".dvc"):
     os.system("dvc config core.no_scm true")
     if os.system("dvc pull train_model") != 0:
         exit("dvc pull failed")
     os.system("rm -r .dvc .apt/usr/lib/dvc")
 
-app = FastAPI()
+app = FastAPI(title="Marcus Census Prediction 🤖",
+              description="Machine Learning trained on the [Census Income Data Set](https://archive.ics.uci.edu/ml/datasets/census+income)")
 model = load("./starter/model/census_classifier.joblib")
 encoder = load("./starter/model/census_encoder.joblib")
 lb = load("./starter/model/census_lb.joblib")
 
 
 class CensusRequest(BaseModel):
+    """Census prediction data model."""
     age: int
     workclass: str
     fnlgt: int
@@ -42,18 +43,53 @@ class CensusRequest(BaseModel):
     native_country: str = Field(alias="native-country")
 
 
-@app.get("/")
-def root():
+class CensusResponse(BaseModel):
+    """Census prediction response data model."""
+    prediction: str
+
+
+@app.get("/", summary="Root path API endpoint", description="Welcome to the Marcus Census Prediction API!")
+async def root():
     return {"message": "Hello World!"}
 
 
-@app.get("/ping")
-def ping():
+@app.get("/ping", summary="Ping API endpoint", description="Ping the API to check if it's up.")
+async def ping():
     return {"ping": "pong!"}
 
 
-@app.post("/predict")
-def predict_census(request: CensusRequest):
+@app.post("/predict", summary="Prediction API endpoint",
+          description="Make a prediction on the census data.",
+          response_model=CensusResponse)
+async def predict_census(request: CensusRequest = Body(default=None, examples={
+    "below": {
+        "summary": "<=50K",
+        "description": "Below 50K",
+        "value": {
+            "age": 39,
+            "workclass": "State-gov",
+            "fnlgt": 77516,
+            "education": "Bachelors",
+            "education-num": 13,
+            "marital-status": "Never-married",
+            "occupation": "Adm-clerical",
+            "relationship": "Not-in-family",
+            "race": "White",
+            "sex": "Male",
+            "capital-gain": 2174,
+            "capital-loss": 0,
+            "hours-per-week": 40,
+            "native-country": "United-States"
+        }},
+    "above": {
+        "summary": ">50K",
+        "description": "Above 50K",
+        "value": {"age": 31, "workclass": "Private", "fnlgt": 45781, "education": "Masters", "education-num": 14,
+                  "marital-status": "Never-married", "occupation": "Prof-specialty", "relationship": "Not-in-family",
+                  "race": "White", "sex": "Female", "capital-gain": 14084, "capital-loss": 0, "hours-per-week": 50,
+                  "native-country": "United-States"}
+    }
+})):
     input_df = pd.DataFrame.from_dict([request.dict(by_alias=True)])
     categorical_features = ['workclass', 'education', 'marital-status', 'occupation',
                             'relationship', 'race', 'sex', 'native-country']

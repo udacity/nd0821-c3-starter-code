347bfd88b8e7510ad6d1978b1bc55ce311dfd239
dell <dell@dell.com>
2022-02-02 00:12:12 +0000

    adding tests for fastapi

--- starter/unit_tests.py
+++ starter/unit_tests.py
@@ -3,6 +3,9 @@ import numpy as np
 from sklearn.utils.validation import check_is_fitted
 from starter.ml.data import process_data
 from starter.ml.model import *
+from fastapi import FastAPI
+from fastapi.testclient import TestClient
+from api import *
 import joblib
 
 
@@ -43,9 +46,68 @@ def test_metrics():
     precision, recall, fbeta = compute_model_metrics(y, preds)
     assert all([(x >=0) & (x <= 1) for x in [precision, recall, fbeta]]), "Metricss failed"    
 
+def test_get():
+    client = TestClient(app)
+    response = client.get("/")
+    assert response.status_code == 200, "Get failed"
+    assert 'greeting' in response.json(), "Get message not found"
+    
+def test_post_pred0():
+    client = TestClient(app)
+    response = client.post(
+        "/",
+        json={
+            "age": 39,
+            "workclass": "State-gov",
+            "fnlgt": 77516,
+            "education": "Bachelors",
+            "education-num": 13,
+            "marital-status": "Never-married",
+            "occupation": "Adm-clerical",
+            "relationship": "Not-in-family",
+            "race": "White",
+            "sex": "Male",
+            "capital-gain": 2174,
+            "capital-loss": 0,
+            "hours-per-week": 40,
+            "native-country": "United-States"
+        },
+    )
+    assert response.status_code == 200, "Post failed"
+    assert response.json()['prediction'] == 0, "Unexpected prediction"
+    
+def test_post_pred1():
+    client = TestClient(app)
+    response = client.post(
+        "/",
+        json={
+            "age": 52,
+            "workclass": "Self-emp-inc",
+            "fnlgt": 287927,
+            "education": "HS-grad",
+            "education-num": 9,
+            "marital-status": "Married-civ-spouse",
+            "occupation": "Exec-managerial",
+            "relationship": "Wife",
+            "race": "White",
+            "sex": "Female",
+            "capital-gain": 15024,
+            "capital-loss": 0,
+            "hours-per-week": 40,
+            "native-country": "United-States"
+        },
+    )
+    assert response.status_code == 200, "Post failed"
+    assert response.json()['prediction'] == 1, "Unexpected prediction"
+
+
+
 
 if __name__ == "__main__":
     test_train()
     test_inference()
     test_metrics()    
+    test_get()
+    test_post_pred0()
+    test_post_pred1()
     print("Everything passed")

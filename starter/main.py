7f3c9f9172eb6e68d8f9561cb5c8533903b028e2
dell <dell@dell.com>
2022-02-03 00:23:08 +0000

    adding heroku apt changes

--- starter/main.py
+++ starter/main.py
@@ -5,6 +5,13 @@ from starter.ml.data import process_data
 from starter.ml.model import *
 import pandas as pd
 import joblib
+import os
+
+if "DYNO" in os.environ and os.path.isdir(".dvc"):
+    os.system("dvc config core.no_scm true")
+    if os.system("dvc pull") != 0:
+        exit("dvc pull failed")
+    os.system("rm -r .dvc .apt/usr/lib/dvc")
 
 # Instantiate the app.
 app = FastAPI()

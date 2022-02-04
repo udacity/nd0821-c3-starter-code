37153924b23ab37609d3f683f26601a496e2125f
dell <dell@dell.com>
2022-02-04 00:02:19 +0000

    adapting import from ml.model

--- starter/starter/ml/model.py
+++ starter/starter/ml/model.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.metrics import fbeta_score, precision_score, recall_score
 from sklearn.ensemble import RandomForestClassifier
-from starter.ml.data import process_data
+from starter.starter.ml.data import process_data
 
 
 # Optional: implement hyperparameter tuning.

260a95744e93b238d6e26efdf4a0c39771e2a5b4
dell <dell@dell.com>
2022-02-04 00:16:49 +0000

    another test to fix imports

--- starter/starter/ml/model.py
+++ starter/starter/ml/model.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.metrics import fbeta_score, precision_score, recall_score
 from sklearn.ensemble import RandomForestClassifier
-from starter.starter.ml.data import process_data
+# from ml.data import process_data
 
 
 # Optional: implement hyperparameter tuning.

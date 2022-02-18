fd35e36b20fb798c87277ee42694c1afb9aed26f
Oliver Schurius <mail@schurius.de>
2022-02-18 10:25:17 +0100

    added stage train LogisticRegression

--- starter/starter/ml/model.py
+++ starter/starter/ml/model.py
@@ -1,15 +1,8 @@
 from sklearn.metrics import fbeta_score, precision_score, recall_score
 from sklearn.metrics import classification_report, RocCurveDisplay
 from sklearn.model_selection import GridSearchCV
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
-
-param_grid = { 
-    'n_estimators': [200, 500],
-    'max_features': ['auto', 'sqrt'],
-    'max_depth' : [4,5,15],
-    'criterion' :['gini', 'entropy']
-}
+from sklearn.ensemble import RandomForestClassifier
 
 # Optional: implement hyperparameter tuning.
 def train_model(X_train, y_train):
@@ -28,15 +21,10 @@ def train_model(X_train, y_train):
         Trained machine learning model.
     """
     # logistic regression
-    # lrc = LogisticRegression(max_iter=1000)
-    # lrc.fit(X_train, y_train)
-    
-    # random forest
-    rfc = RandomForestClassifier(random_state=42)
-    cv_rfc = GridSearchCV(estimator=rfc, param_grid=param_grid, cv=5)
-    cv_rfc.fit(X_train, y_train)
-    
-    return cv_rfc.best_estimator_, str("RainForest")
+    lrc = LogisticRegression(max_iter=1000)
+    lrc.fit(X_train, y_train)
+
+    return lrc, str("LogisticRegression")
 
 
 def compute_model_metrics(y, preds):

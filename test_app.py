0ea27191635233a3b49aceaa25eb85efb68fcea1
saraselis <saras1@hotmail.com.br>
2022-04-10 20:46:28 -0300

    test_predict_response

--- test_app.py
+++ test_app.py
@@ -34,33 +34,6 @@ def test_hello3():
     assert response.status_code == HTTPStatus.OK
 
 
-# @pytest.mark.parametrize('test_input', [
-#     ('age', "Age of the person - numerical - int"),
-#     ('fnlgt', 'MORE INFO NEEDED - numerical - int'),
-#     ('race', 'Race of the person - nominal categorical - str')
-# ])
-# def test_feature_info_status(test_input: str):
-#     response = client.get(f'/feature_info/{test_input}')
-#     assert response.status_code == HTTPStatus.OK
-#     assert response.request.method == "GET"
-
-
-# @pytest.mark.parametrize('test_input, expected', [
-#     ('age', "Age of the person - numerical - int"),
-#     ('fnlgt', 'MORE INFO NEEDED - numerical - int'),
-#     ('race', 'Race of the person - nominal categorical - str')
-# ])
-# def test_feature_info_response(test_input: str, expected: str):
-#     """Testing features
-
-#     Args:
-#         test_input (np.array): input test
-#         expected (np.array): results expected
-#     """
-#     response = client.get(f'/feature_info/{test_input}')
-#     assert response.json() == expected
-
-
 def test_predict_status1():
     """
     Tests POST route predict.
@@ -107,7 +80,7 @@ def test_predict_status2():
     assert response.status_code == HTTPStatus.OK
 
 
-def test_predict_response():
+def test_predict_response1():
     data = {
     "age": 55,
     "workclass": "Private",
@@ -126,21 +99,36 @@ def test_predict_response():
     }
 
     response = client.post("/model", json=data)
-    # assert response.json()['label'] == 0 or response.json()['label'] == 1
-    # assert response.json()['prob'] >= 0 and response.json()['label'] <= 1
-    # assert response.json()['salary'] == ' >50k' or response.json()[
-        # 'salary'] == ' <=50k'
     assert response.json()["Result"] == 1
 
 
-# def test_missing_feature_predict():
-#     """
-#     Testing post route for missing predict.
-#     """
-#     data = {
-#         "age": 0
-#     }
-#     response = client.post("/model/", json=data)
-#     assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
-#     assert response.request.method == "POST"
-#     assert response.json()["detail"][0]["type"] == "value_error.missing"
+def test_predict_response0():
+    data = {"age": 55,
+            "workclass": "Private",
+            "fnlgt": 77516,
+            "education": "Masters",
+            "education-num": 13,
+            "marital-status": "Never-married",
+            "occupation": "Adm-clerical",
+            "relationship": "Not-in-family",
+            "race": "White",
+            "sex": "Female",
+            "capital-gain": 2174,
+            "capital-loss": 0,
+            "hours-per-week": 40,
+            "native-country": "United-States"
+            }
+
+    response = client.post("/model", json=data)
+    assert response.json()["Result"] == 0
+
+
+def test_missing_feature_predict():
+    """
+    Testing post route for missing predict.
+    """
+    data = {
+        "age": 0
+    }
+    response = client.post("/model", json=data)
+    assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY

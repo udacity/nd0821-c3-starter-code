8d7dafae58b375e9662d0fa7e6e5489a064972b9
Ayoubkx <ayoub@ayoub.localdomain>
2022-05-11 13:18:51 +0300

    training model

--- starter/starter/ml/model.py
+++ starter/starter/ml/model.py
@@ -1,4 +1,12 @@
 from sklearn.metrics import fbeta_score, precision_score, recall_score
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
+from sklearn.linear_model import LogisticRegression
+from ml.data import process_data
+import csv
+import logging
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
+logger = logging.getLogger()
 
 
 # Optional: implement hyperparameter tuning.
@@ -17,9 +25,15 @@ def train_model(X_train, y_train):
     model
         Trained machine learning model.
     """
+    rf_pipe = RandomForestClassifier()
+    logger.info("Fitting")
+    rf_pipe.fit(X_train,y_train)
 
-    pass
+    lr_pipe = LogisticRegression()
+    lr_pipe.fit(X_train, y_train)
 
+    return rf_pipe, lr_pipe
+    
 
 def compute_model_metrics(y, preds):
     """
@@ -57,4 +71,51 @@ def inference(model, X):
     preds : np.array
         Predictions from the model.
     """
-    pass
+    preds = model.predict(X)
+    return preds
+
+def slice_performance(model, data, encoder, lb, save_path):
+    """ Outputs the performance of the model on slices of the data into a csv file.
+    Inputs
+    ------
+    model : ???
+        Trained machine learning model.
+    data : pd.Dataframe
+        Data to be sliced and used for model prediction.
+    encoder : sklearn.preprocessing._encoders.OneHotEncoder
+        Trained sklearn OneHotEncoder, only used if training=False.
+    lb : sklearn.preprocessing._label.LabelBinarizer
+        Trained sklearn LabelBinarizer, only used if training=False.
+    save_path : String 
+        Absolute path and filename to save the output )
+    """
+    cat_features = [
+        "workclass",
+        "education",
+        "marital_status",
+        "occupation",
+        "relationship",
+        "race",
+        "sex",
+        "native_country",
+    ]
+    slice_performance = []
+
+    for category in cat_features:
+        unique_values = data[category].unique()
+        for value in unique_values:
+            slice_data = data[data[category] == value]
+            X_slice, y_slice, encoder, lb = process_data(
+                slice_data, categorical_features=cat_features, label="salary", training=False, encoder=encoder, lb=lb
+            )
+            y_slice_pred = inference(model, X_slice)
+            precision, recall, fbeta = compute_model_metrics(y_slice, y_slice_pred)
+            slice_performance.append((category, value, precision, recall, fbeta))
+            slice_performance.append('\n')
+
+    with open(save_path, 'w', newline='') as file:
+        wr = csv.writer(file, quoting=csv.QUOTE_ALL)
+        wr.writerow(slice_performance)
+
+
+    return 0
--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -1,10 +1,21 @@
 # Script to train machine learning model.
-
+import os
+import pandas as pd
+import pickle
+import logging
+from sklearn import linear_model
+from ml.model import train_model, inference, compute_model_metrics, slice_performance
+from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split
+from ml.data import process_data
+
 
 # Add the necessary imports for the starter code.
-
+logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
+logger = logging.getLogger()
 # Add code to load in the data.
+path_to_data = os.path.join(os.getcwd(), "data", "clean_data.csv")
+data = pd.read_csv(path_to_data)
 
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
 train, test = train_test_split(data, test_size=0.20)
@@ -24,5 +35,37 @@ X_train, y_train, encoder, lb = process_data(
 )
 
 # Proces the test data with the process_data function.
-
+X_test, y_test, encoder, lb = process_data(
+    test, categorical_features=cat_features, label="salary", training=False, encoder=encoder, lb=lb
+)
+with open(os.path.join(os.getcwd(), "starter", "model", "label_binarizer.pkl"), 'wb') as f:
+    pickle.dump(lb, f)
+with open(os.path.join(os.getcwd(), "starter", "model", "encoder.pkl"), 'wb') as f:
+    pickle.dump(encoder, f)
 # Train and save a model.
+
+logger.info("Train model")
+rf_model, lr_model = train_model(X_train, y_train)
+logger.info("Save model")
+with open(os.path.join(os.getcwd(), "starter", "model", "rf_model.pkl"), 'wb') as file:
+    pickle.dump(rf_model, file)
+
+y_pred = inference(rf_model, X_test)
+
+print(y_pred)
+logger.info("Scoring")
+r_squared = rf_model.score(X_test, y_test)
+
+mae = mean_absolute_error(y_test, y_pred)
+
+logger.info(f"Score: {r_squared}")
+logger.info(f"MAE: {mae}")
+
+precision, recall, fbeta = compute_model_metrics(y_test, y_pred)
+logger.info(f"precision: {precision}")
+logger.info(f"recall: {recall}")
+logger.info(f"fbeta: {fbeta}")
+
+slice_performance(rf_model, data, encoder, lb, os.path.join(os.getcwd(), "starter", "model", "slice_performance_rf.csv"))
+
+

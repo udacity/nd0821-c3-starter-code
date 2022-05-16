f3daf5440a7443ab93883ee3fe4a0b595c4511c6
Ayoubkx <ayoub@ayoub.localdomain>
2022-05-16 14:37:39 +0300

    model card

--- starter/main.py
+++ starter/main.py
@@ -1,1 +1,80 @@
-# Put the code for your API here.
+from typing import Optional, Union
+import pandas as pd
+import os
+import yaml
+
+from fastapi import FastAPI
+from pydantic import BaseModel, Field
+
+from starter.ml.model import load_model, inference
+from starter.ml.data import process_data
+
+if "DYNO" in os.environ and os.path.isdir(".dvc"):
+    os.system("dvc config core.no_scm true")
+    if os.system("dvc pull") != 0:
+        exit("dvc pull failed")
+    os.system("rm -r .dvc .apt/usr/lib/dvc")
+
+
+class CensusItem(BaseModel):
+    age: Optional[Union[int, list]] = [39, 52]
+    workclass: Optional[Union[str, list]] = ['State-gov', 'Self-emp-inc']
+    fnlgt: Optional[Union[int, list]] = [77516, 287927]
+    education: Optional[Union[str, list]] = ['Bachelors', 'HS-grad']
+    education_num: Optional[Union[int, list]] = Field([13, 9], alias='education-num')
+    marital_status: Optional[Union[str, list]] = Field(['Never-married', 'Married-civ-spouse'], alias='marital-status')
+    occupation: Optional[Union[str, list]] = ['Adm-clerical', 'Exec-managerial']
+    relationship: Optional[Union[str, list]] = ['Not-in-family', 'Wife']
+    race: Optional[Union[str, list]] = ['White', 'White']
+    sex: Optional[Union[str, list]] = ['Male', 'Female']
+    capital_gain: Optional[Union[int, list]] = Field([2174, 15024], alias='capital-gain')
+    capital_loss: Optional[Union[int, list]] = Field([0, 0], alias='capital-loss')
+    hours_per_week: Optional[Union[int, list]] = Field([40, 40], alias='hours-per-week')
+    native_country: Optional[Union[str, list]] = Field(['United-States', 'United-States'], alias='native-country')
+
+    class Config:
+        allow_population_by_field_name = True
+        
+
+root_path = os.path.dirname(os.path.abspath(__file__))
+model = load_model(root_path, 'model.pkl')
+preprocessor = load_model(root_path, 'preprocessor.pkl')
+
+with open(os.path.join(root_path, "starter", "constants.yaml"), 'r') as f:
+    categorical_features = yaml.safe_load(f)["categorical_features"]
+
+app = FastAPI()
+
+@app.get("/")
+async def welcome_message():
+    return {'Greetings From Aakash': 'Welcome To Udacity Project on Model Deployment'}
+
+
+@app.post("/model_inference")
+async def model_inference(data: CensusItem):
+    data_dic = data.dict(by_alias=True)
+
+    for key, value in data_dic.items():
+        if not isinstance(value, list):
+            data_dic[key] = [value]
+
+    df = pd.DataFrame(data_dic)
+
+    data_processed, _, _, _ = process_data(
+        df, categorical_features=categorical_features, label=None, training=False,
+        preprocessor=preprocessor, label_binarizer=None
+    )
+
+    pred = list(inference(model, data_processed))
+
+    for idx, val in enumerate(pred):
+        if pred[idx] == 0:
+            pred[idx] = '<=50K'
+        else:
+            pred[idx] = '>50K'
+
+    return {'Prediction': pred}
+
+
+
+
--- starter/model_card_template.md
+++ starter/model_card_template.md
@@ -3,10 +3,30 @@
 For additional information see the Model Card paper: https://arxiv.org/pdf/1810.03993.pdf
 
 ## Model Details
+The used model is a Random Forest classifier using the default parameters. The model is used to predict if someone is making more than 50 K in salary based on the following attributes
+
+age
+workclass
+fnlgt
+education
+education-num
+marital-status
+occupation
+relationship
+race
+sex
+capital-gain
+capital-loss
+hours-per-week
+native-country
+salary
 
 ## Intended Use
 
+The model should be used to predict the salary based on a set of attributes. The user of this model are students of the a Udacity Nano Degree Program. 
+
 ## Training Data
+Data can be found here Census Income data set and locally in /data/census.csv
 
 ## Evaluation Data
 
@@ -14,5 +34,6 @@ For additional information see the Model Card paper: https://arxiv.org/pdf/1810.
 _Please include the metrics used and your model's performance on those metrics._
 
 ## Ethical Considerations
-
+The data used to train the model might contain biases. Some attributes that are collected from conducting a survey, such as hours per week, might contain biases based on influence from friends, co-workers or expectations. Not all countries are represented in the native country attribute and the data set is probably not large enough to assume that the model predicts well with native country attribute.
 ## Caveats and Recommendations
+A very simple Random Forest model with default parameters was trained on the data set and no effort was put into training a model with better performance since the Udacity projects was more about CI/CD. The model would also perform better with a larger data set.

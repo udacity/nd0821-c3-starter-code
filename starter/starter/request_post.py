598f934013f66bee6117f7d2dcf18be0d6d2b578
Ayoubkx <ayoub@ayoub.localdomain>
2022-05-25 19:56:02 +0300

    requirements

---
+++ starter/starter/request_post.py
@@ -0,0 +1,20 @@
+import requests
+import json
+
+attributes = {
+    "age": 45,
+    "workclass": "Private",
+    "fnlgt": 280464,
+    "education": "Doctorate",
+    "education_num": 10,
+    "marital_status": "Married-civ-spouse",
+    "occupation": "Exec-managerial",
+    "relationship": "Husband",
+    "race": "White",
+    "sex": "Male",
+    "capital_gain": 510,
+    "capital_loss": 0,
+    "hours_per_week": 60,
+    "native_country": "United-States"
+}
+

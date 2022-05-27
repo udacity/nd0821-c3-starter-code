cfb6cdd61a8cf29c22b7b6468abfa124c3a1a469
Ayoubkx <ayoub@ayoub.localdomain>
2022-05-27 15:26:01 +0300

    test

--- test_model.py
+++ test_model.py
@@ -1,56 +1,55 @@
-from sklearn.linear_model import LogisticRegression
 
+import os
+import numpy as np
+import logging
 from starter.starter.ml.model import load_model
 from starter.starter.ml.model import train_model, inference
 from starter.starter.ml.model import compute_model_metrics, compute_slice_metrics
+from starter.starter.ml.data import process_data
 
-import pandas as pd
-
-
-def test_load_model(root_path):
-    model = load_model(root_path, "model.pkl")
-
-    assert isinstance(model, LogisticRegression)
-    assert model.max_iter == 300
-    assert model.n_features_in_ == 108
+logging.basicConfig(level=logging.INFO,
+                    format="%(asctime)-15s %(message)s")
+logger = logging.getLogger()
 
 
-def test_train_model(data):
-    X, y = data
-    model = train_model(X, y)
 
-    assert isinstance(model, LogisticRegression)
-    assert model.max_iter == 300
-    assert model.n_features_in_ == 4
+def test_columns_names(data):
+    expected_columns = [
+        "age",
+        "workclass",
+        "fnlwgt",
+        "education",
+        "education-num",
+        "marital-status",
+        "occupation",
+        "relationship",
+        "race",
+        "sex",
+        "capital-gain",
+        "capital-loss",
+        "hours-per-week",
+        "native-country",
+        "income"
+    ]
 
-
-def test_inference(model, data):
-    X, y = data
-    y_pred = inference(model, X)
-
-    assert len(y_pred) == len(y)
-    assert y_pred.any() == 1
-
-
-def test_compute_model_metrics(model, data):
-    X, y = data
-    y_pred = inference(model, X)
-
-    precision, recall, fbeta = compute_model_metrics(y, y_pred)
-
-    assert precision > 0.0
-    assert recall > 0.0
-    assert fbeta > 0.0
+    assert list(expected_columns) == list(data.columns.values), \
+        logger.info("Column names in the input data doesn't match")
 
 
-def test_compute_slice_metrics(data):
-    X, y = data
-    slice_performance = compute_slice_metrics(
-        features=X,
-        labels=y,
-        predictions=y,
-        cat_features=['A']
-    )
+def test_age_range(data, min_age=0, max_age=100):
+    """To check if there is an outlier in the age"""
 
-    assert isinstance(slice_performance, pd.DataFrame)
-    assert (slice_performance.columns == ['Precision', 'Recall', 'TNR', 'NPV', 'F-Score']).all()
+    idx = data['age'].between(min_age, max_age)
+
+    assert np.sum(~idx) == 0, \
+        logger.info("Age column has outliers.")
+
+def test_relationship_category(data):
+    known_relationship_values = [
+        'Wife', 'Own-child', 'Husband', 'Not-in-family', 'Other-relative', 'Unmarried']
+
+    relationships = set(data['relationship'].unique())
+    logger.info(f'known relations: {relationships}')
+    # Unordered check
+    assert set(known_relationship_values) == set(relationships), \
+        logger.info("relationships values in the input data doesn't match")

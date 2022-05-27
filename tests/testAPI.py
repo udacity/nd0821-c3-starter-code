a98dce13a0936e3b7bc9037eaf67a537bc7eef5c
Ayoubkx <ayoub@ayoub.localdomain>
2022-05-27 13:04:09 +0300

    test

---
+++ tests/testAPI.py
@@ -0,0 +1,29 @@
+from fastapi.testclient import TestClient
+
+from starter.main import app
+
+
+client = TestClient(app)
+
+
+def test_get_path():
+    r = client.get("/")
+
+    assert r.status_code == 200
+    assert r.json() == {'Greetings From aakash': 'Welcome To Udacity Project on Model Deployment'}
+
+
+def test_post_model_inference_zero_class(train_data):
+    zero_class = train_data[0]
+    r = client.post("/model_inference", json=zero_class.to_dict())
+
+    assert r.status_code == 200
+    assert r.json() == {'Prediction': ['<=50K']}
+
+
+def test_post_model_inference_one_class(train_data):
+    one_class = train_data[1]
+    r = client.post("/model_inference", json=one_class.to_dict())
+
+    assert r.status_code == 200
+    assert r.json() == {'Prediction': ['>50K']}

afc55f2d6bc0a2e91992809381c52ef557b55b2b
Zaid Ghazal <zghazal@beyond.ai>
2022-10-22 01:00:23 +0300

    Add tests

---
+++ starter/test/test_api.py
@@ -0,0 +1,28 @@
+"""This module includes API testing functions"""
+
+import requests
+
+
+def test_read_root():
+    """Test the root path"""
+    response = requests.get("http://localhost:8000/")
+    assert response.status_code == 200
+    assert response.json() == {"msg": "Welcome to the API"}
+
+def test_inference_precision():
+    """Test the inference path"""
+    response = requests.post("http://localhost:8000/model_inference", json={"model_name": "model_1"})
+    assert response.status_code == 200
+    assert isinstance(response.json()["precision"], float)
+
+def test_inference_recall():
+    """Test the inference path"""
+    response = requests.post("http://localhost:8000/model_inference", json={"model_name": "model_1"})
+    assert response.status_code == 200
+    assert isinstance(response.json()["recall"], float)
+
+def test_inference_fbeta():
+    """Test the inference path"""
+    response = requests.post("http://localhost:8000/model_inference", json={"model_name": "model_1"})
+    assert response.status_code == 200
+    assert isinstance(response.json()["fbeta"], float)
--- starter/test/test_model.py
+++ starter/test/test_model.py
@@ -1,14 +1,13 @@
 from typing import Any
 from pytest import fixture
-from data import process_data, import_data 
-from model import train_model, compute_model_metrics, inference
+from starter.ml.data import process_data, import_data 
+from starter.ml.model import train_model, compute_model_metrics, inference
 from sklearn.datasets import make_classification
 from yaml import load, Loader
 import pandas as pd
 import numpy as np
 
-from starter.starter.train_model import X_test, X_train
-paths_dict = load(open("starter/path.yml"), Loader=Loader)
+paths_dict = load(open("../starter/path.yml"), Loader=Loader)
 DATA_PATH = paths_dict["DATA_PATH"]
 
 @fixture(scope='session')
@@ -21,8 +20,31 @@ def data() -> pd.DataFrame:
         Dataframe containing the data.
     """
     data = import_data(DATA_PATH)
+    data.columns = data.columns.str.strip()
     return data
 
+@fixture(scope='session')
+def model(data: pd.DataFrame) -> Any:
+    """ Train and save ML model create to predict the salary of a person based on their demographic features.
+    Inputs
+    ------
+    data : pd.DataFrame
+        Dataframe containing the data.
+
+    Returns
+    -------
+    model : Any
+        Model object.
+    """    
+    X, y = make_classification(n_samples=400, n_features=2, n_informative=2, n_redundant=0, random_state=0)
+    model = train_model(X, y)
+
+    assert model is not None, "Model should not be None."
+
+    return model
+    
+
+    
 
 def test_data_validity(data: pd.DataFrame):
     """ Test that the data is valid. """
@@ -37,39 +59,17 @@ def test_compute_model_metrics(data: pd.DataFrame):
     data : pd.DataFrame
         Dataframe containing the data.
     """
-    y = data['salary']
-    preds = data['salary']*1.5
+    y = [1,0,0,0,1,1,0,1,1,0]
+    preds = [0,0,0,0,1,1,0,1,1,0]
     precision, recall, fbeta = compute_model_metrics(y, preds)
 
     assert isinstance(precision, float), "Precision should be a float."
     assert isinstance(recall, float), "Recall should be a float."
     assert isinstance(fbeta, float), "Fbeta should be a float."
 
-@fixture(scope='session')
-def test_train_model(data):
-    """ Test that the model is valid.
-    Inpputs
-    -------
-    data : pd.DataFrame
-        Dataframe containing the data
-    
-    Returns
-    -------
-    model : Any
-        Model object.
-    X_test : np.ndarray
-        Test data.
-    """
-    X, y = make_classification(n_samples=400, n_features=2, n_informative=2, n_redundant=0, random_state=0)
-    X_train, y_train, X_test, y_test = X[:300], y[:300], X[300:], y[300:]
-    model = train_model(X_train, y_train)
 
-    assert model is not None, "Model should not be None."
 
-    return model, X_test
-    
-
-def test_inference(model: Any, X_test: np.ndarray):
+def test_inference(model: Any):
     """ Test that the inference is valid.
     Inpputs
     -------
@@ -78,6 +78,7 @@ def test_inference(model: Any, X_test: np.ndarray):
     X_test : np.ndarray
         Test data.
     """
+    X_test,_ = make_classification(n_samples=100, n_features=2, n_informative=2, n_redundant=0, random_state=0)
     predictions = inference(model, X_test)
 
     assert type(predictions) is np.ndarray, "Predictions should be a numpy array."
@@ -85,7 +86,7 @@ def test_inference(model: Any, X_test: np.ndarray):
     
     
     
-    
+
     
     
     

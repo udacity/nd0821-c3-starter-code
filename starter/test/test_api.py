a0558cd1673da2d54289a8b3a363fb162428ff9f
Zaid Ghazal <zghazal@beyond.ai>
2022-10-22 03:22:17 +0300

    edit api test

--- starter/test/test_api.py
+++ starter/test/test_api.py
@@ -1,7 +1,19 @@
 """This module includes API testing functions"""
 
 import requests
+from pytest import fixture
 
+
+@fixture(scope='session')
+def metrics():
+    """Test the inference path"""
+    response = requests.post(
+        "http://localhost:8000/model_inference",
+        json={
+            "model_name": "model_1"})
+    assert response.status_code == 200
+
+    return response.json()["precision"], response.json()["recall"], response.json()["fbeta"]
 
 def test_read_root():
     """Test the root path"""
@@ -9,32 +21,19 @@ def test_read_root():
     assert response.status_code == 200
     assert response.json() == {"msg": "Welcome to the API"}
 
-
-def test_inference_precision():
+def test_inference_precision(metrics):
     """Test the inference path"""
-    response = requests.post(
-        "http://localhost:8000/model_inference",
-        json={
-            "model_name": "model_1"})
-    assert response.status_code == 200
-    assert isinstance(response.json()["precision"], float)
+    
+    assert isinstance(metrics[0], float)
 
 
-def test_inference_recall():
+def test_inference_recall(metrics):
     """Test the inference path"""
-    response = requests.post(
-        "http://localhost:8000/model_inference",
-        json={
-            "model_name": "model_1"})
-    assert response.status_code == 200
-    assert isinstance(response.json()["recall"], float)
+    
+    assert isinstance(metrics[1], float)
 
 
-def test_inference_fbeta():
+def test_inference_fbeta(metrics):
     """Test the inference path"""
-    response = requests.post(
-        "http://localhost:8000/model_inference",
-        json={
-            "model_name": "model_1"})
-    assert response.status_code == 200
-    assert isinstance(response.json()["fbeta"], float)
+    
+    assert isinstance(metrics[2], float)

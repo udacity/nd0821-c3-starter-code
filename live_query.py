80b820a23a162b53a9d5ce606964667ae4732bd6
Zaid Ghazal <zghazal@beyond.ai>
2022-10-22 12:02:14 +0300

    add photos and live query module

---
+++ live_query.py
@@ -0,0 +1,13 @@
+import requests
+
+
+def get_model_inference():
+    url = "https://ml-ci-cd-project.herokuapp.com/model_inference"
+    response = requests.post(url, json={"model_name": "model_1"})
+    return  {"Status Code": response.status_code, "Precision": response.json()["precision"],
+     "Recall": response.json()["recall"], "Fbeta": response.json()["fbeta"]}
+    
+
+
+if __name__ == "__main__":
+    print(get_model_inference())

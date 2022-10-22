097a508091b30af7a196589448c387e8b0a247f0
Zaid Ghazal <zghazal@beyond.ai>
2022-10-22 12:20:59 +0300

    ignore

--- live_query.py
+++ live_query.py
@@ -4,9 +4,11 @@ import requests
 def get_model_inference():
     url = "https://ml-ci-cd-project.herokuapp.com/model_inference"
     response = requests.post(url, json={"model_name": "model_test"})
-    return  {"Status Code": response.status_code, "Precision": response.json()["precision"],
-    "Recall": response.json()["recall"], "Fbeta": response.json()["fbeta"]}
-    
+    if response.status_code == 200:
+        return  {"Status Code": response.status_code, "Precision": response.json()["precision"],
+        "Recall": response.json()["recall"], "Fbeta": response.json()["fbeta"]}
+    else:
+        return {"Status Code": response.status_code}
 
 
 if __name__ == "__main__":

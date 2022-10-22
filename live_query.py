cb197eb10ebf84343dd497efad711c74312b5182
Zaid Ghazal <zghazal@beyond.ai>
2022-10-22 12:09:54 +0300

    include live

--- live_query.py
+++ live_query.py
@@ -3,9 +3,9 @@ import requests
 
 def get_model_inference():
     url = "https://ml-ci-cd-project.herokuapp.com/model_inference"
-    response = requests.post(url, json={"model_name": "model_1"})
+    response = requests.post(url, json={"model_name": "model_test"})
     return  {"Status Code": response.status_code, "Precision": response.json()["precision"],
-     "Recall": response.json()["recall"], "Fbeta": response.json()["fbeta"]}
+    "Recall": response.json()["recall"], "Fbeta": response.json()["fbeta"]}
     
 
 

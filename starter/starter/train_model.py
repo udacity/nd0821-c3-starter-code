46e6fd1288d987e75039b0a2ca19ab26f52c9b79
DSgUY <kenji.nakasone@correo.ucu.edu.uy>
2023-01-30 06:21:37 -0300

    update

--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -1,11 +1,20 @@
 # Script to train machine learning model.
 
+import os
 from sklearn.model_selection import train_test_split
+import pandas as pd
 
 # Add the necessary imports for the starter code.
+from ml.data import make_dataset
 
 # Add code to load in the data.
+# Fist we generate the processed dataset.csv from the raw census.csv
+make_dataset(os.path.join('starter', 'data', 'census.csv'),
+             os.path.join('starter', 'data', 'dataset.csv'))
 
+# Then, we load the processed datset
+data = pd.read_csv(os.path.join('starter', 'data', 'dataset.csv'))
+                   
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
 train, test = train_test_split(data, test_size=0.20)
 

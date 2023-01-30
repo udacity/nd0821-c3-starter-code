fb13bcaeb032eddace4753596bff18bcaea3da45
DSgUY <kenji.nakasone@correo.ucu.edu.uy>
2023-01-30 15:07:58 -0300

    update

--- starter/main.py
+++ starter/main.py
@@ -1,1 +1,55 @@
 # Put the code for your API here.
+import pickle
+import os
+from pandas import DataFrame
+from fastapi import FastAPI, HTTPException
+from .api.schema import ModelInput
+from .starter.ml.model import load_model, load_encoder, load_lb, inference
+from .starter.ml.data import process_data
+
+# load model
+api_model = load_model('model_dtc.pkl')
+
+# Categorical Features
+cat_features = [
+    "workclass",
+    "education",
+    "marital_status",
+    "occupation",
+    "relationship",
+    "race",
+    "sex",
+    "native_country",
+]
+
+# Instantiate the app.
+app = FastAPI()
+
+# Define a GET on the specified endpoint.
+@app.get("/")
+async def say_hello():
+    return {"greeting": "Hello World!"}
+
+
+@app.post("/predict")
+async def predict(input_data: ModelInput):
+    X_input = DataFrame([input_data.dict()])
+
+    # Proces the test data with the process_data function.
+    encoder = load_encoder('encoder_dtc.pkl')
+    lb = load_lb('lb_dtc.pkl')
+
+    # Run: process data
+    X_infer, _, _, _ = process_data(
+        X_input,
+        categorical_features=cat_features,
+        encoder=encoder,
+        lb=lb,
+        training=False,
+    )
+
+    # Run:inference
+    pred = inference(model=api_model, X=X_infer)
+
+    # Run: inverse of the binarizer to get: "<=50K" or "">50K"
+    return lb.inverse_transform(pred)[0]

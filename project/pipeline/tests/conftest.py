4bc493c0e5359b5b43710026827020a4736551eb
BialyMT92 <mt.bialek92@gmial.com>
2023-03-15 14:40:37 +0100

    Test_model done

---
+++ project/pipeline/tests/conftest.py
@@ -0,0 +1,62 @@
+import pandas as pd
+import pytest
+import joblib
+import numpy as np
+
+from sklearn.model_selection import train_test_split
+from project.pipeline.ml.data import process_data, import_data
+from project.pipeline.ml.model import compute_model_metrics, inference
+
+@pytest.fixture(scope='session')
+def data():
+    try:
+        data = import_data("../../data/clean_census.csv")
+        return data
+    except FileNotFoundError as err:
+        pytest.fail("Testing import data: File wasn't found")
+
+@pytest.fixture(scope='session')
+def model():
+    try:
+        model = joblib.load("../../model/rfc_model.pkl")
+        return model
+    except FileNotFoundError as err:
+        pytest.fail("Testing import model: File wasn't found")
+
+@pytest.fixture(scope='session')
+def lb():
+    try:
+        model = joblib.load("../../model/lb.pkl")
+        return model
+    except FileNotFoundError as err:
+        pytest.fail("Testing import lb: File wasn't found")
+
+@pytest.fixture(scope='session')
+def encoder():
+    try:
+        model = joblib.load("../../model/encoder.pkl")
+        return model
+    except FileNotFoundError as err:
+        pytest.fail("Testing import encoder: File wasn't found")
+
+@pytest.fixture(scope='session')
+def testprocess(data, encoder, lb):
+    cat_features = [
+        "workclass",
+        "education",
+        "marital-status",
+        "occupation",
+        "relationship",
+        "race",
+        "sex",
+        "native-country",
+    ]
+    try:
+        train, test = train_test_split(data, test_size=0.20)
+        X_train, y_train, encoder, lb = process_data(train, categorical_features=cat_features, label="salary",
+                                                     training=True)
+        X_test, y_test, _, _ = process_data(test, categorical_features=cat_features, label="salary", training=False,
+                                            encoder=encoder, lb=lb)
+        return [X_test, y_test]
+    except AssertionError as err:
+        raise err
---
+++ project/pipeline/tests/test_model.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import pytest
+import numpy as np
+
+from project.pipeline.ml.data import process_data, import_data
+from project.pipeline.ml.model import compute_model_metrics, inference
+
+
+def test_column_names(data):
+    expected_columns = [
+        "workclass",
+        "education",
+        "marital-status",
+        "occupation",
+        "relationship",
+        "race",
+        "sex",
+        "native-country",
+        "salary"
+    ]
+
+    these_columns = data.columns.values
+
+    # This also enforces the same order
+    assert all(item in these_columns for item in expected_columns), "Expected columns is present in dataset"
+
+
+def test_inference(model, testprocess):
+    pred = inference(model, testprocess[0])
+    assert isinstance(pred, np.ndarray), "Prediction is NOT a numpy array, check your data"
+
+
+def test_compute_model_metrics(model, testprocess):
+    pred = inference(model, testprocess[0])
+    precision, recall, fbeta = compute_model_metrics(testprocess[1], pred)
+    assert isinstance(precision, float), "Precision is NOT float"
+    assert isinstance(recall, float), "Recall is NOT float"
+    assert isinstance(fbeta, float), "Fbeta is NOT float"

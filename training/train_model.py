b8103d62318241e0c4bbc39900a9e8e8f4544492
Ilona <ilona.brinkmeier@gmx.net>
2023-08-21 14:54:12 +0200

    Fix: comment code out for github actions

--- training/train_model.py
+++ training/train_model.py
@@ -7,7 +7,7 @@ from sklearn.model_selection import train_test_split
 # Add code to load in the data.
 
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
-train, test = train_test_split(data, test_size=0.20)
+#train, test = train_test_split(data, test_size=0.20)
 
 cat_features = [
     "workclass",
@@ -19,9 +19,9 @@ cat_features = [
     "sex",
     "native-country",
 ]
-X_train, y_train, encoder, lb = process_data(
-    train, categorical_features=cat_features, label="salary", training=True
-)
+#X_train, y_train, encoder, lb = process_data(
+#    train, categorical_features=cat_features, label="salary", training=True
+#)
 
 # Proces the test data with the process_data function.
 
